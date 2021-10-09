b7f9e768a9038f8902a2c7b302c37406d470d9fc
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2021-10-09 15:35:07 +0200

    feat: validate types with support for nesting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export function hello(name: string): string {
-  return `hello, ${name}`
+import get from 'lodash.get'
+import set from 'lodash.set'
+import { nameof } from 'ts-simple-nameof'
+
+export default function validate<T>(
+  object: T,
+  properties: {
+    prop: (obj: T) => string
+    validations: ((param) => boolean)[]
+  }[]
+): ValidationResult<T> {
+  const result: ValidationResult<T> = {}
+  properties.forEach((propertyValidation) => {
+    set(
+      result,
+      nameof<T>(propertyValidation.prop),
+      propertyValidation.validations.map((validation) =>
+        validation(get(object, nameof<T>(propertyValidation.prop)))
+      )
+    )
+  })
+  return result
 }
+
+export type ValidationResult<T> =
+  | {
+      [K in keyof T]?: T[K] extends Record<string, unknown>
+        ? ValidationResult<T[K]>
+        : boolean[]
+    }
+  | never
---
+++ test/validator_tests.ts
@@ -0,0 +1,63 @@
+import 'should'
+
+import { default as v } from 'validator'
+
+import validate from '../src/index'
+
+type ContactPerson = {
+  firstName: string
+  lastName: string
+  email: string
+}
+
+type Customer = {
+  companyName: string
+  contactPerson: ContactPerson
+}
+
+const notNullOrUndefined = (str): boolean =>
+  !(str === null || typeof str === 'undefined')
+
+describe('validator', () => {
+  it('should validate types', (done) => {
+    const sut: ContactPerson = {
+      firstName: 'Jane',
+      lastName: 'Doe',
+      email: null
+    }
+    const result = validate<ContactPerson>(sut, [
+      {
+        prop: (c) => c.email,
+        validations: [
+          (value) => notNullOrUndefined(value),
+          (value) => value && v.isEmail(value)
+        ]
+      }
+    ])
+    result.email[0].should.be.false()
+    done()
+  })
+
+  it('should validate nested types', (done) => {
+    const sut: Customer = {
+      companyName: 'Acme Corp.',
+      contactPerson: {
+        firstName: 'Jane',
+        lastName: 'Doe',
+        email: 'jane.doe@acme.com'
+      }
+    }
+    const result = validate<Customer>(sut, [
+      {
+        prop: (p) => p.contactPerson.email,
+        validations: [
+          (value) => notNullOrUndefined(value),
+          (value) => v.isEmail(value)
+        ]
+      }
+    ])
+    result.contactPerson.email[0].should.be.true()
+    result.contactPerson.email[1].should.be.true()
+    done()
+  })
+})
